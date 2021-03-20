91540ddfce1b93c3bfed0cb20065c9328e3e9295
Giovani Moutinho <giovanim@ymail.com>
2021-03-20 15:20:13 -0300

    Move conftest files

--- reseller_cashback_api/apps/conftest.py
+++ reseller_cashback_api/apps/conftest.py
@@ -1,11 +1,19 @@
 import pytest
-from model_bakery import baker
 from rest_framework.test import APIClient
+from model_bakery import baker
 
-from ..models import Reseller
+from .reseller.models import Reseller
+from .order.models import Order
 
 pytestmark = pytest.mark.django_db
 
+@pytest.fixture
+def order_data():
+    return {
+        'code': 'ABC-123',
+        'price': '123.45',
+        'date': '2021-03-20',
+    }
 
 @pytest.fixture
 def reseller_data():
@@ -18,12 +26,16 @@ def reseller_data():
 
 
 @pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
 def fixed_reseller(reseller_data):
     reseller = baker.make(Reseller, **reseller_data)
     reseller.set_password(reseller.password)
     reseller.save()
     return reseller
-
 
 @pytest.fixture
 def reseller():
@@ -31,5 +43,10 @@ def reseller():
 
 
 @pytest.fixture
-def api_client():
-    return APIClient()
+def fixed_order(order_data, fixed_reseller):
+    return baker.make(Order, **order_data, reseller=fixed_reseller)
+
+
+@pytest.fixture
+def order(reseller):
+    return baker.make(Order, reseller=reseller)
