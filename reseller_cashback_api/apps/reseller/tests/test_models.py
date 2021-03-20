2c38b80d7816338f51bcc5a9fd18af80216f3c08
Giovani Moutinho <giovanim@ymail.com>
2021-03-20 09:19:49 -0300

    Replace model mommy with model bakery

--- reseller_cashback_api/apps/reseller/tests/test_models.py
+++ reseller_cashback_api/apps/reseller/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.contrib.auth.models import User
-from model_mommy import mommy
+from model_bakery import baker
 
 from ..models import Reseller
 
@@ -9,15 +9,15 @@ from ..models import Reseller
 class ResellerModelTestCase(TestCase):
 
     def setUp(self):
-        self.user = mommy.make(User, username='user', email='test@a.com',
+        self.user = baker.make(User, username='user', email='test@a.com',
                                is_staff=False, is_superuser=False)
 
     def test_reseller_model_can_be_created(self):
         reseller = Reseller.objects.create(
             name='John Smith', cpf='123.456.789-90', user=self.user)
         self.assertTrue(isinstance(reseller, Reseller))
-        self.assertEquals(reseller.name, 'John Smith')
-        self.assertEquals(reseller.cpf, '123.456.789-90')
+        self.assertEqual(reseller.name, 'John Smith')
+        self.assertEqual(reseller.cpf, '123.456.789-90')
 
     def test_reseller_model_cannot_be_created_without_name(self):
         reseller = Reseller.objects.create(
